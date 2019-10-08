f40c32e4f09945f5e3eed676d1ab88b29d846e5b
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-08 17:06:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -8,12 +8,7 @@ def my_each(array)
       i = i + 1 
     end
    array
-  else
-    puts "Hey! No block was given!"
+ 
   end
 end
 
-#collection = [1, 2, 3, 4]
-#my_each(collection) do |i|
-# puts i
-#end
